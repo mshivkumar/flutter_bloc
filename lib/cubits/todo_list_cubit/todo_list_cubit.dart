9e2d9d3f6dfbdeee604ffea4d0043f1e96ece207
Shiv Maurya <shiv@MacBook-Pro.local>
2023-01-15 12:15:37 +0530

    added todo list cubit

---
+++ lib/cubits/todo_list_cubit/todo_list_cubit.dart
@@ -0,0 +1,45 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../models/todo_model.dart';
+
+part 'todo_list_state.dart';
+
+class TodoListCubit extends Cubit<TodoListState> {
+  TodoListCubit() : super(TodoListState.initial());
+
+  void addTodo({required String newDesc}) {
+    final Todo newTodo = Todo(desc: newDesc);
+    final List<Todo> newTodos = [...state.todos, newTodo];
+    emit(state.copyWith(todos: newTodos));
+  }
+
+  void editTodo({required String id, required String newDesc}) {
+    final List<Todo> newTodosList = state.todos.map((todo) {
+      if (todo.id == id) {
+        return Todo(id: todo.id, desc: newDesc, isCompleted: todo.isCompleted);
+      }
+
+      return todo;
+    }).toList();
+
+    emit(state.copyWith(todos: newTodosList));
+  }
+
+  void toggleStatus({required String id, required bool isCompleted}) {
+    final List<Todo> newTodoList = state.todos.map((todo) {
+      if (todo.id == id) {
+        return Todo(id: todo.id, desc: todo.desc, isCompleted: isCompleted);
+      }
+
+      return todo;
+    }).toList();
+  }
+
+  void deleteTodo({required String id}) {
+    List<Todo> newTodosList = state.todos.where((todo) => todo.id != id).toList();
+    // newTodosList.removeWhere((todo) => todo.id == id);
+
+    emit(state.copyWith(todos: newTodosList));
+  }
+}
---
+++ lib/cubits/todo_list_cubit/todo_list_state.dart
@@ -0,0 +1,39 @@
+part of 'todo_list_cubit.dart';
+
+class TodoListState extends Equatable {
+  final List<Todo> todos;
+
+  const TodoListState({
+    required this.todos,
+  });
+
+  factory TodoListState.initial() {
+    return const TodoListState(todos: []);
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is TodoListState &&
+          runtimeType == other.runtimeType &&
+          todos == other.todos);
+
+  @override
+  int get hashCode => todos.hashCode;
+
+  @override
+  String toString() {
+    return 'TodoListState{ todos: $todos,}';
+  }
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(
+      todos: todos ?? this.todos,
+    );
+  }
+
+  @override
+  List<Object> get props => [todos];
+}
