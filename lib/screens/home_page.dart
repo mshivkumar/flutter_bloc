f2024faed1bf47f086adc7f554edeef3e3722d90
Shiv Maurya <shiv@MacBook-Pro.local>
2023-01-20 22:52:53 +0530

    added the sekelton

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('Home Page'),
+        ),
+      ),
+    );
   }
 }
