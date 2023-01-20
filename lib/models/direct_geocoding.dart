68facdc22f710149cae4eca2b8c0c578a1461f25
Shiv Maurya <shiv@MacBook-Pro.local>
2023-01-21 00:09:13 +0530

    added direct geocoding model class

---
+++ lib/models/direct_geocoding.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+class DirectGeocoding extends Equatable {
+  final String name;
+  final double lat;
+  final double lon;
+  final String country;
+
+  const DirectGeocoding({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.country,
+  });
+
+  @override
+  String toString() {
+    return 'DirectGeocoding{name: $name, lat: $lat, lon: $lon, country: $country}';
+  }
+
+  @override
+  List<Object> get props => [name, lat, lon, country];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'lat': lat,
+      'lon': lon,
+      'country': country,
+    };
+  }
+
+  factory DirectGeocoding.fromMap(List<Map<String, dynamic>> json) {
+    final Map<String, dynamic> data = json[0];
+    return DirectGeocoding(
+      name: data['name'] as String,
+      lat: data['lat'] as double,
+      lon: data['lon'] as double,
+      country: data['country'] as String,
+    );
+  }
+}
